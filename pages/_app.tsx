33dfd6d3eeae557c5ddad4c588a400c717f16a6c
huewilliams <hswspwgod@gmail.com>
2022-05-31 17:52:31 +0900

    feat: emotion theme provider 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import {ThemeProvider} from "@emotion/react"
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import theme from "../styles/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+      </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ styles/theme.ts
@@ -0,0 +1,5 @@
+const theme = {
+    testColor: '#1E90FF',
+}
+
+export default theme;
