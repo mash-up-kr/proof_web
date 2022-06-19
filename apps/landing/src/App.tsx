69d303ca2072d0214116531e6110f8393f27fd54
otterji <youngsdg1@gmail.com>
2022-06-19 18:03:58 +0900

    feat: setup react & next

---
+++ apps/landing/src/App.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function App() {
+  return <h1>HEELOWORLD</h1>;
+}
+
+export default App;
---
+++ apps/service/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <h1>HEELOW NEXT</h1>;
+};
+
+export default Home;
