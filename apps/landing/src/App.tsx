3d28c465c8bd31f210c1441b982578392c984667
otterji <youngsdg1@gmail.com>
2022-06-19 19:34:47 +0900

    chore: test vercel deploy

--- apps/landing/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-function App() {
-  return <h1>HEELOWORLD</h1>;
-}
-
-export default App;
--- apps/service/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const Home: NextPage = () => {
-  return <h1>HEELOW NEXT</h1>;
-};
-
-export default Home;
