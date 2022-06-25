8c75fb399beb660b42e7dad6829f1be88b24e480
otterji <youngsdg1@gmail.com>
2022-06-25 16:54:08 +0900

    feat: next amplitude init 수정

--- apps/service/src/pages/_app.tsx
+++ apps/service/src/pages/_app.tsx
@@ -4,7 +4,9 @@ import { initAmplitude } from "hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
-    initAmplitude("799f4fce304d7b04dc47c3480b34486a");
+    if (typeof window !== "undefined") {
+      initAmplitude("799f4fce304d7b04dc47c3480b34486a");
+    }
   }, []);
 
   return <Component {...pageProps} />;
