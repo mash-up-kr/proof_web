fbbece88d5bd02af71e867555000d46663f6200a
zi-gae <doscmdev@gmail.com>
2022-06-25 20:20:59 +0900

    feat: eslint

--- apps/service/src/pages/index.tsx
+++ apps/service/src/pages/index.tsx
@@ -1,5 +1,23 @@
+const componnents = [
+  {
+    id: 1,
+    name: "Home",
+  },
+  {
+    id: 2,
+    name: "About",
+  },
+];
+
 const Home = ({ id }: { id: string }) => {
-  return <h1>ID: {id}</h1>;
+  return (
+    <>
+      <h1>ID: {id}</h1>
+      {componnents.map((componnent) => (
+        <div></div>
+      ))}
+    </>
+  );
 };
 
 export async function getServerSideProps() {
