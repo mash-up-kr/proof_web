a065907a7d78455f47f97c05229c8e2cceeb2b8f
Lee <43488305+poiu694@users.noreply.github.com>
2022-06-25 20:38:36 +0900

    chore: eslint 규칙 추가

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,26 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  plugins: ['react', 'react-hooks', 'prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  extends: ['prettier'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "error",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
+          },
+        ],
+      },
+    ],
   },
 };
