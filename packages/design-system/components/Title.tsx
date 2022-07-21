5b89f1628bcc68ef6ac14bb10fe992b6a11a8464
Lee <43488305+poiu694@users.noreply.github.com>
2022-07-21 13:00:06 +0900

    feat: Add title component

---
+++ packages/design-system/components/Title.tsx
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import React, { CSSProperties } from "react";
+import theme from "../theme";
+import Text from "./Text";
+
+interface Title {
+  topQuestion: string;
+  bottomQuestion: string;
+  desc: string;
+}
+
+interface TitleProps extends React.HTMLAttributes<HTMLDivElement> {
+  textAlign?: CSSProperties["textAlign"];
+}
+
+function Title({
+  topQuestion,
+  bottomQuestion,
+  desc,
+  textAlign,
+  ...restProps
+}: Title & TitleProps) {
+  return (
+    <Wrapper {...restProps}>
+      <QuestionWrapper>
+        <Text type="h1" color={theme.palette.white} textAlign={textAlign}>
+          {topQuestion}
+        </Text>
+        <Text type="h1" color={theme.palette.white} textAlign={textAlign}>
+          {bottomQuestion}
+        </Text>
+      </QuestionWrapper>
+      <Text type="body2" color={theme.palette.gray200} textAlign={textAlign}>
+        {desc}
+      </Text>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.padding.sm}px;
+`;
+
+const QuestionWrapper = styled.div``;
+
+export default Title;
