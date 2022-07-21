6cf029684a90b31c41eff2ef26b1a323893e783e
Lee <43488305+poiu694@users.noreply.github.com>
2022-07-21 12:19:18 +0900

    feat: Apply global style

--- apps/service/src/pages/_app.tsx
+++ apps/service/src/pages/_app.tsx
@@ -3,8 +3,8 @@ import type { AppProps } from "next/app";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "@emotion/react";
-import { theme } from "design-system";
+import { Global, ThemeProvider } from "@emotion/react";
+import { globalStyle, theme } from "design-system";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <Global styles={globalStyle} />
         <ReactQueryDevtools />
         <Hydrate state={pageProps.dehydratedState}>
           <RecoilRoot>
