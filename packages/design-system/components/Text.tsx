3112d528bd770a2e038bf0c4c20076fe0eab6305
Lee <43488305+poiu694@users.noreply.github.com>
2022-08-02 00:01:57 +0900

    fix: fix duplicate name Text

--- packages/design-system/components/Text.tsx
+++ packages/design-system/components/Text.tsx
@@ -1,26 +1,24 @@
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
-import React, { CSSProperties, PropsWithChildren } from 'react';
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import React, { CSSProperties, PropsWithChildren } from "react";
 
-import theme, { FontKeyType, PalleteValueType } from '../theme';
+import theme, { FontKeyType, PalleteValueType } from "../theme";
 
-interface Text extends React.HTMLAttributes<HTMLDivElement> {
+interface TextProps extends React.HTMLAttributes<HTMLDivElement> {
   type: FontKeyType;
-  display?: CSSProperties['display'];
-  textAlign?: CSSProperties['textAlign'];
+  display?: CSSProperties["display"];
+  textAlign?: CSSProperties["textAlign"];
   color?: PalleteValueType;
 }
-
-interface TextProps extends PropsWithChildren<Text> {}
 
 function Text({
   type,
   children,
-  display = 'block',
-  textAlign = 'left',
+  display = "block",
+  textAlign = "left",
   color = theme.colors.text.general,
   ...restProps
-}: TextProps) {
+}: PropsWithChildren<TextProps>) {
   return (
     <Wrapper
       type={type}
