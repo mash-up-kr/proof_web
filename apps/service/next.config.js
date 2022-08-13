aa6174ef90a02251fce17d55eb638901acb749df
otterji <youngsdg1@gmail.com>
2022-08-13 17:57:29 +0900

    chore: ignore typescript build error

--- apps/service/next.config.js
+++ apps/service/next.config.js
@@ -3,6 +3,9 @@ const withTM = require("next-transpile-modules")(["hooks", "design-system"]);
 
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 }
 
 module.exports = withTM(nextConfig)
