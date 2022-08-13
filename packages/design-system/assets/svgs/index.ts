f18895ee9a4c60261dfdce285aadb575a151a33f
Lee <43488305+poiu694@users.noreply.github.com>
2022-08-13 18:38:44 +0900

    feat: add default svgs for icon

--- packages/design-system/assets/svgs/index.ts
+++ packages/design-system/assets/svgs/index.ts
@@ -1,2 +1,7 @@
+export { default as Check } from "./Check";
 export { default as DirectionRight } from "./DirectionRight";
 export { default as Search } from "./Search";
+export { default as TypeA } from "./TypeA";
+export { default as TypeB } from "./TypeB";
+export { default as Vote } from "./Vote";
+export { default as Winner } from "./Winner";
