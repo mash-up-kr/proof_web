b10e5181113e580eeadecd227f904d6f74a43bab
Lee <43488305+poiu694@users.noreply.github.com>
2022-08-16 00:05:48 +0900

    feat: Add Round Select Card

---
+++ apps/service/src/components/RoundCard.tsx
@@ -0,0 +1,77 @@
+import styled from "@emotion/styled";
+import { Icon, Text, theme } from "design-system";
+
+interface Round {
+  title: string;
+  count: number;
+}
+
+interface RoundCardProps extends React.HTMLAttributes<HTMLLIElement> {
+  round: Round;
+  isActive?: boolean;
+}
+
+function RoundCard({ round, isActive = false, ...restProps }: RoundCardProps) {
+  return (
+    <Wrapper isActive={isActive} {...restProps}>
+      <RoundImage
+        src={`/round${round.count}.png`}
+        alt={`round ${round.count} icon`}
+        isActive={isActive}
+      />
+      <Description>
+        <Text type="body3" color={theme.palette.gray200}>
+          {round.title}
+        </Text>
+        <Text type="h3" color={theme.colors.text.special}>
+          {round.count}강
+        </Text>
+      </Description>
+      <RightIcon name="directionRight" />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.li<{ isActive: boolean }>`
+  width: 100%;
+  padding: ${theme.padding.md}px;
+  margin: 0 auto;
+  border-radius: 8px;
+  background-color: ${(props) => {
+    if (props.isActive) {
+      return props.theme.palette.purple300;
+    }
+    return props.theme.palette.gray500;
+  }};
+  position: relative;
+  display: flex;
+  align-items: flex-start;
+  gap: 12px;
+  box-sizing: border-box;
+`;
+
+const RoundImage = styled.img<{ isActive: boolean }>`
+  width: 52px;
+  height: 52px;
+  border-radius: 50%;
+  object-fit: contain;
+  background-color: ${(props) => {
+    if (props.isActive) return "rgba(28, 28, 38, 0.7)";
+    return "transparent";
+  }};
+`;
+
+const RightIcon = styled(Icon)`
+  position: absolute;
+  top: 50%;
+  right: 28px;
+  transform: translateY(-50%);
+`;
+
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export default RoundCard;
--- apps/service/src/components/index.ts
+++ apps/service/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as DrinkImage } from "./DrinkImage";
 export { default as DrinkCard } from "./DrinkCard";
 export { default as Header } from "./Header";
+export { default as RoundCard } from "./RoundCard";
