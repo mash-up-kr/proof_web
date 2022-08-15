ef2dcdc7171377efb37d772c7f51a49791f8cb59
Lee <43488305+poiu694@users.noreply.github.com>
2022-08-15 16:44:10 +0900

    feat: Add worldcup sample page

---
+++ apps/service/src/pages/worldcup.tsx
@@ -0,0 +1,102 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { BottomButton, Text } from "design-system";
+import * as React from "react";
+import DrinkCard, { Drink } from "../components/DrinkCard";
+
+const dummy1: Drink = {
+  id: 2,
+  name: "버번카운티 브랜드 스타우트 2018",
+  abv: 15.2,
+  imageUrl:
+    "https://beer-api-prod.idkulab.com/media/drinks/bourbon-county-2018.png",
+  category: {
+    id: 2,
+    name: "Beer",
+    imageUrl:
+      "https://zuzu-resource.s3.ap-northeast-2.amazonaws.com/drinks-category/category_beer.png",
+  },
+};
+const dummy2: Drink = {
+  id: 3,
+  name: "펑크 아이피에이",
+  abv: 5.6,
+  imageUrl:
+    "https://beer-api-prod.idkulab.com/media/drinks/brewdong-punk-ipa.png",
+  category: {
+    id: 2,
+    name: "Beer",
+    imageUrl:
+      "https://zuzu-resource.s3.ap-northeast-2.amazonaws.com/drinks-category/category_beer.png",
+  },
+};
+
+const WorldCup = () => {
+  const [isActive, setIsActive] = React.useState<boolean[]>([false, false]);
+
+  const handleClickDrinkCard = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    const id = Number(e.currentTarget.id);
+    const nextIsActive = [...isActive];
+    nextIsActive[+!id] = false;
+    nextIsActive[id] = !nextIsActive[id];
+    setIsActive(nextIsActive);
+  };
+
+  return (
+    <Layout>
+      <TitleWrapper>
+        <Text type="h1" textAlign="center">
+          비 오는 날씨엔
+        </Text>
+        <Text type="h1" textAlign="center">
+          어떤 술이 더 끌리나요?
+        </Text>
+      </TitleWrapper>
+      <CandidateWrapper>
+        <DrinkCard
+          id="0"
+          type="typeA"
+          drink={dummy1}
+          isActive={isActive[0]}
+          onClick={handleClickDrinkCard}
+        />
+        <DrinkCard
+          id="1"
+          type="typeB"
+          drink={dummy2}
+          isActive={isActive[1]}
+          onClick={handleClickDrinkCard}
+        />
+      </CandidateWrapper>
+      <BottomButton isActive={isActive[0] || isActive[1]}>
+        선택하기
+      </BottomButton>
+    </Layout>
+  );
+};
+
+const Layout = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
+
+    background-color: ${theme.colors.background};
+  `}
+`;
+
+const TitleWrapper = styled.div`
+  padding-top: 52px;
+`;
+
+const CandidateWrapper = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  gap: 18px;
+`;
+
+export default WorldCup;
