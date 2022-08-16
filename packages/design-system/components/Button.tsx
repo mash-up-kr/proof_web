af9a2dd66324fef81efa3497a32c478fe99237fc
zi-gae <doscmdev@gmail.com>
2022-08-16 21:24:57 +0900

    feat: 컴포넌트 수정 및 디폴트 값 추가

--- packages/design-system/components/Button.tsx
+++ packages/design-system/components/Button.tsx
@@ -2,21 +2,38 @@ import styled from "@emotion/styled";
 import React, { CSSProperties } from "react";
 import theme from "../theme";
 
+// TODOS: 타입 유틸으로 분리
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
+type XOR<T, U> = T | U extends Record<string, unknown>
+  ? (Without<T, U> & U) | (Without<U, T> & T)
+  : T | U;
+
 export enum ButtonHierarchy {
   Primary = "Primary",
   Secondary = "Secondary",
+  Tertiary = "Tertiary",
 }
 
-export interface ButtonProps {
-  hierarchy: ButtonHierarchy;
-  children: React.ReactNode;
-  margin?: CSSProperties["margin"];
-  width?: CSSProperties["width"];
-  className?: string;
-  fullWidth?: boolean;
-  disabled?: boolean;
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => unknown;
-}
+type ButtonProps = XOR<
+  {
+    fullWidth?: boolean;
+    hierarchy?: ButtonHierarchy;
+    children: React.ReactNode;
+    margin?: CSSProperties["margin"];
+    className?: string;
+    disabled?: boolean;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => unknown;
+  },
+  {
+    width?: CSSProperties["width"];
+    hierarchy?: ButtonHierarchy;
+    children: React.ReactNode;
+    margin?: CSSProperties["margin"];
+    className?: string;
+    disabled?: boolean;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => unknown;
+  }
+>;
 
 function Button(props: ButtonProps) {
   return <ButtonWrapper {...props}>{props.children}</ButtonWrapper>;
@@ -24,16 +41,20 @@ function Button(props: ButtonProps) {
 
 const ButtonWrapper = styled.button<ButtonProps>`
   padding: 15px;
-  width: ${(props) =>
-    typeof props.width === "number" ? props.width + "px" : props.width};
+  width: ${({ width, fullWidth }) => {
+    if (width) return typeof width === "number" ? width + "px" : width;
+    if (fullWidth) return "100%";
+    return "auto";
+  }};
   margin: ${(props) => props.margin};
-  border-radius: ${(props) => (props.fullWidth ? "0" : "8px")};
-  color: ${(props) =>
-    props.disabled ? theme.palette.gray300 : theme.colors.text.special};
-  background-color: ${(props) => {
-    if (props.hierarchy === ButtonHierarchy.Secondary)
-      return theme.palette.gray400;
-    else if (props.disabled) return theme.palette.gray500;
+  border-radius: 8px;
+  color: ${({ disabled }) =>
+    disabled ? theme.palette.gray300 : theme.colors.text.special};
+  background-color: ${({ hierarchy, disabled }) => {
+    if (hierarchy === ButtonHierarchy.Secondary) return theme.palette.gray400;
+    if (hierarchy === ButtonHierarchy.Tertiary || disabled)
+      return theme.palette.gray500;
+
     return theme.colors.primary.default;
   }};
   outline: none;
