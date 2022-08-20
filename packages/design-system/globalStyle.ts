5e867ab94da78935706d7d0b90aa36ea793ce2ba
Lee <43488305+poiu694@users.noreply.github.com>
2022-08-20 20:11:02 +0900

    feat: Apply pretendard fonts

--- packages/design-system/globalStyle.ts
+++ packages/design-system/globalStyle.ts
@@ -6,12 +6,35 @@ import { colors } from "./theme";
 const globalStyle = css`
   ${emotionReset}
 
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 700;
+    src: url("/fonts/Pretendard-Bold.woff");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 600;
+    src: url("/fonts/Pretendard-SemiBold.woff");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 500;
+    src: url("/fonts/Pretendard-Medium.woff");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 400;
+    src: url("/fonts/Pretendard-Regular.woff");
+  }
+
   body {
     width: 100%;
     height: 100vh;
     height: calc(var(--vh, 1vh) * 100);
     background-color: ${colors.background};
     color: ${colors.text.general};
+
+    font-family: Pretendard;
   }
 
   button {
