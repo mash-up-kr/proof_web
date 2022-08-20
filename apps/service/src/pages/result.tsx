e7ee9fd29bc04ee631ec8911bf64c61a61bc33df
otterji <youngsdg1@gmail.com>
2022-08-20 23:13:41 +0900

    feat: 공유하기 안될 시 클립보드로 공유하기 조건 추가

--- apps/service/src/pages/result.tsx
+++ apps/service/src/pages/result.tsx
@@ -2,10 +2,13 @@ import styled from "@emotion/styled";
 import { Text } from "design-system";
 import * as React from "react";
 import { NextPage, NextPageContext } from "next";
+import { useRouter } from "next/router";
 import { FloatingButtons, Header, Rankings } from "../components";
 import { ROUNDS } from "../dummy/rounds";
 import { DRINK_CARDS } from "../dummy/drinkCards";
 import WinnerCard from "../components/WinnerCard";
+
+const BASE_URL = `https://zuzu-web.vercel.app`;
 
 interface Props {
   userAgent: string;
@@ -13,6 +16,7 @@ interface Props {
 
 // FIXME:: 타입 추론
 const Result: NextPage<Props> = ({ userAgent }) => {
+  const router = useRouter();
   const [isFromNativeApp, setIsFromNativeApp] = React.useState<boolean>(false);
   const [isShared, setIsShared] = React.useState<boolean>(false);
 
@@ -31,14 +35,16 @@ const Result: NextPage<Props> = ({ userAgent }) => {
       <WinnerCard drink={DRINK_CARDS[3]} />
       <FloatingButtons
         handleClickRightButton={async () => {
-          alert("눌렸어요");
-          console.log(navigator.share);
           if (navigator.share) {
             await navigator.share({
-              title: "프루프 테스트",
-              text: "Hello World",
-              url: "https://github.com/mash-up-kr/proof_web",
+              title: "Proof",
+              text: "분위기에 취하고 맛에 취하는 우리, 프루프에서 술드컵을 진행해보세요. 자세한 내용은 다음 초대 링크에서 확인하세요.",
+              url: `${BASE_URL}${router.pathname}`,
             });
+          } else if (navigator.clipboard) {
+            navigator.clipboard
+              .writeText(`${BASE_URL}${router.pathname}`)
+              .then(() => alert("링크가 클립보드에 복사되었습니다."));
           } else {
             alert("공유하기가 지원되지 않는 환경 입니다.");
           }
