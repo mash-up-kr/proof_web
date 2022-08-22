9acef44143980b6276f3573580a33866b95d97da
otterji <youngsdg1@gmail.com>
2022-08-22 22:15:46 +0900

    feat: 공유하기 기능 추가

--- apps/service/src/pages/result.tsx
+++ apps/service/src/pages/result.tsx
@@ -7,6 +7,7 @@ import { FloatingButtons, Header, Rankings } from "../components";
 import { ROUNDS } from "../dummy/rounds";
 import { DRINK_CARDS } from "../dummy/drinkCards";
 import WinnerCard from "../components/WinnerCard";
+import share from "../utils/share";
 
 const BASE_URL = `https://zuzu-web.vercel.app`;
 
@@ -24,6 +25,12 @@ const Result: NextPage<Props> = ({ userAgent }) => {
   //   // TODO: setIsFromNativeApp & setIsShared 설정, n초 후 BottomSheet
   // }, []);
 
+  const shareData = {
+    title: "Proof",
+    text: "분위기에 취하고 맛에 취하는 우리, 프루프에서 술드컵을 진행해보세요. 자세한 내용은 다음 초대 링크에서 확인하세요.",
+    url: `${BASE_URL}${router.pathname}`,
+  };
+
   return (
     <>
       <Header type="prev" title="결과" />
@@ -35,18 +42,11 @@ const Result: NextPage<Props> = ({ userAgent }) => {
       <WinnerCard drink={DRINK_CARDS[3]} />
       <FloatingButtons
         handleClickRightButton={async () => {
-          if (navigator.share) {
-            await navigator.share({
-              title: "Proof",
-              text: "분위기에 취하고 맛에 취하는 우리, 프루프에서 술드컵을 진행해보세요. 자세한 내용은 다음 초대 링크에서 확인하세요.",
-              url: `${BASE_URL}${router.pathname}`,
-            });
-          } else if (navigator.clipboard) {
-            navigator.clipboard
-              .writeText(`${BASE_URL}${router.pathname}`)
-              .then(() => alert("링크가 클립보드에 복사되었습니다."));
-          } else {
-            alert("공유하기가 지원되지 않는 환경 입니다.");
+          const result = await share(shareData);
+          if (result === "copiedToClipboard") {
+            alert("링크를 클립보드에 복사했습니다.");
+          } else if (result === "failed") {
+            alert("공유하기가 지원되지 않는 환경입니다.");
           }
         }}
       />
