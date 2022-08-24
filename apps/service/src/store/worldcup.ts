b7b5027c5b887fb723862d37a2d31dd92958b345
Lee <43488305+poiu694@users.noreply.github.com>
2022-08-25 01:31:12 +0900

    fix: Add WithWhoType with using atom

--- apps/service/src/store/worldcup.ts
+++ apps/service/src/store/worldcup.ts
@@ -1,31 +1,23 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
 import { DrinkWithRound } from "../components/DrinkCard";
+
+export type WithWhoType = "SOLO" | "DUO";
 
 export interface WorldCup {
   currentIndex: number;
   currentRound: number;
   totalRound: number;
-  with: "alone" | "group";
+  with: WithWhoType;
   situation: string;
   drinks: DrinkWithRound[];
 }
-
-const worldCupAtom = atom<WorldCup>({
-  key: "worldCup",
-  default: {
-    currentIndex: 0,
-    currentRound: 8,
-    totalRound: 8,
-    with: "alone",
-    situation: "",
-    drinks: [],
-  },
-});
 
 /**
  * currentRound: 현재 진행중인 라운드
  *
  * currentIndex: 현재 진행중인 라운드에서 가지는 인덱스
+ *
+ * totalRound: 유저가 선택한 라운드
  *
  * with: 누구랑 같이 먹나요?
  *
@@ -33,13 +25,14 @@ const worldCupAtom = atom<WorldCup>({
  *
  * drinks: 선택한 라운드에 따라 백엔드에서 가져오는 주류 정보 리스트
  */
-export const worldCupState = selector<WorldCup>({
-  key: "worldCupState",
-  get: ({ get }) => {
-    const { ...state } = get(worldCupAtom);
-    return { ...state };
-  },
-  set: ({ set }, nextState) => {
-    set(worldCupAtom, nextState);
+export const worldCupState = atom<WorldCup>({
+  key: "worldCup",
+  default: {
+    currentIndex: 0,
+    currentRound: 0,
+    totalRound: 0,
+    with: "SOLO",
+    situation: "",
+    drinks: [],
   },
 });
