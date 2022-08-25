4ce5ca34c6196798905592f7849a925ab6aca196
Lee <43488305+poiu694@users.noreply.github.com>
2022-08-25 13:48:11 +0900

    feat: react-query hook with service api

---
+++ apps/service/src/api/query/WorldCupQuery.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "react-query";
+import { WorldCupService } from "../service";
+
+const WORLDCUP_KEY = "worldcup";
+const WITH_WHO_KEY = "with_who";
+const DRINKS_KEY = "drinks";
+
+export const useGetOneWorldCupInfoById = (worldCupId: number) =>
+  useQuery([WORLDCUP_KEY], () => WorldCupService.getOneWorldcupInfo({ worldCupId }));
+
+export const useGetWorldCupInfosByCategorizingWithWho = () =>
+  useQuery([WORLDCUP_KEY, WITH_WHO_KEY], () =>
+    WorldCupService.getWorldCupInfosByCategorizingWithWho()
+  );
+
+export const useGetDrinks = (worldCupId: number, round: number) => {
+  useQuery([WORLDCUP_KEY, DRINKS_KEY], () =>
+    WorldCupService.getDrinksByRound({ worldCupId, round })
+  );
+};
