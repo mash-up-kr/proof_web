c41fd7b35d72723a1577f7b6854d90f5739b7160
Lee <43488305+poiu694@users.noreply.github.com>
2022-09-01 00:26:27 +0900

    design: SearchIcon 백그라운드만 투명도 처리

--- packages/design-system/assets/svgs/Search.tsx
+++ packages/design-system/assets/svgs/Search.tsx
@@ -1,25 +1,26 @@
 import React from "react";
+import theme from "../../theme";
 
 const Search = ({ ...restProps }: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...restProps}
     >
       <path
-        d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
-        stroke="#777777"
+        d="M7.33333 12.6667C10.2789 12.6667 12.6667 10.2789 12.6667 7.33333C12.6667 4.38781 10.2789 2 7.33333 2C4.38781 2 2 4.38781 2 7.33333C2 10.2789 4.38781 12.6667 7.33333 12.6667Z"
+        stroke={theme.palette.gray50}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M21 21L16.65 16.65"
-        stroke="#777777"
+        d="M14.0001 14.0001L11.1001 11.1001"
+        stroke={theme.palette.gray50}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
