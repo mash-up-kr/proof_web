7aae97d44e0fceba2b08fc9ceed679900e8ab142
Lee <43488305+poiu694@users.noreply.github.com>
2022-09-02 16:40:14 +0900

    test: token 확인 테스트

--- apps/service/src/utils/native/action.js
+++ apps/service/src/utils/native/action.js
@@ -1,8 +1,22 @@
 import proof from "./core";
 
-export const getProofAccessToken = (options, callback) => {
-  proof.callNative("getProofAccessToken", options, callback);
+export const getProofAccessToken = (options = {}) => {
+  return new Promise((resolve, reject) => {
+    proof.callNative(
+      "getProofAccessToken",
+      options,
+      (statusCode, msg, response) => {
+        if (statusCode !== 200) {
+          reject(new Error());
+        }
+        const responseObj = JSON.parse(JSON.stringify(response));
+        console.log(responseObj);
+        resolve(responseObj.accessToken ?? "");
+      }
+    );
+  });
 };
+
 
 export const closeWebView = (options, callback) => {
   proof.callNative("closeWebView", options, callback);
