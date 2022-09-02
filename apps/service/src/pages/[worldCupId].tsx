9ed90f09d0d3ed29e6302057dde018ef729b551f
Lee <43488305+poiu694@users.noreply.github.com>
2022-09-02 23:20:17 +0900

    feat: WorldCup 아이디가 선택되어서 왔을 때 대응할 페이지 제작

---
+++ apps/service/src/pages/[worldCupId].tsx
@@ -0,0 +1,105 @@
+import { ParsedUrlQuery } from "querystring";
+import styled from "@emotion/styled";
+import { BottomButton, Title } from "design-system";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import * as React from "react";
+import { useSetRecoilState } from "recoil";
+import { WorldCupService } from "../api/service";
+
+import { Header } from "../components";
+import { useNavigate } from "../hooks";
+import { worldCupState } from "../store";
+
+interface Params extends ParsedUrlQuery {
+  worldCupId: string;
+}
+
+type WorldCupIdProps = Pick<Params, "worldCupId">;
+
+const HomeWithWorldCupId: NextPage<WorldCupIdProps> = ({ worldCupId }) => {
+  const navigate = useNavigate();
+
+  const setWorldCupState = useSetRecoilState(worldCupState);
+
+  React.useEffect(() => {
+    if (typeof worldCupId !== "undefined") {
+      setWorldCupState((prev) => ({
+        ...prev,
+        worldCupId: Number(worldCupId),
+      }));
+    }
+  }, [setWorldCupState, worldCupId]);
+
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:title"
+          content="매력적인 술꾼! 당신의 술 취향을 증명할 수 있도록 초대장이 도착했어요."
+          key="proof"
+        />
+        <meta
+          property="og:image"
+          content="https://zuzu-resource.s3.ap-northeast-2.amazonaws.com/proof_logo.png"
+        />
+      </Head>
+      <Wrapper>
+        <Header type="logo" onClickIcon={navigate.toNativeHome} />
+        <MainTitle
+          title={"술 취향 증명을\n시작해볼까요?"}
+          desc="당신의 취향을 증명할 술들을 준비했어요."
+          textAlign="center"
+        />
+        <MainImage
+          src="/worldcup_start.png"
+          alt="worldcup-main-image"
+          width="280px"
+          height="324px"
+        />
+        <BottomButton isActive onClick={() => navigate.push("/round")}>
+          시작하기!
+        </BottomButton>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.main`
+  text-align: center;
+`;
+
+const MainTitle = styled(Title)`
+  margin-top: 20px;
+`;
+
+const MainImage = styled.img`
+  margin-top: 38px;
+  border-radius: 8px;
+`;
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  const worldcups = await WorldCupService.getAllWorldcupInfo();
+  const paths = worldcups.map((worldcup) => ({
+    params: { worldCupId: String(worldcup.id) },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<WorldCupIdProps, Params> = async (
+  context
+) => {
+  const { worldCupId } = context.params!;
+
+  return {
+    props: {
+      worldCupId,
+    },
+  };
+};
+
+export default HomeWithWorldCupId;
