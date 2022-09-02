2f5f6182de7aa766687e72c4ac0483040db0cf47
Lee <43488305+poiu694@users.noreply.github.com>
2022-09-02 13:45:15 +0900

    feat: DrinkEvaluation Query 필요한 부분 추가 및 수정

--- apps/service/src/api/query/DrinkEvaluationQuery.ts
+++ apps/service/src/api/query/DrinkEvaluationQuery.ts
@@ -1,17 +1,26 @@
-import { useQueries } from "react-query";
+import { useQueries, useQuery } from "react-query";
+import { DrinkWithRound } from "../../components/DrinkCard";
 import { DrinkEvaluationService, DrinkService } from "../service";
 
 const DRINK_KEY = "drink";
 const DRINK_EVALUATION_KEY = "drink_evaluation";
 
-export const useGetDrinkInfoAndEvaluationById = (id: number) =>
-  useQueries([
-    {
-      queryKey: [DRINK_KEY, id],
-      queryFn: () => DrinkService.getOneDrinkInfo({ id }),
-    },
-    {
-      queryKey: [DRINK_EVALUATION_KEY, id],
-      queryFn: () => DrinkEvaluationService.getDrinksEvaluation({ id }),
-    },
-  ]);
+export const useGetDrinkInfoById = (id: number) =>
+  useQuery({
+    queryKey: [DRINK_KEY, id],
+    queryFn: () => DrinkService.getOneDrinkInfo({ id }),
+  });
+
+export const useGetDrinkEvaluationById = (id: number) =>
+  useQuery([DRINK_EVALUATION_KEY, id], () =>
+    DrinkEvaluationService.getDrinksEvaluation({ id })
+  );
+
+export const useGetDrinksEvaluationById = (drinks: DrinkWithRound[]) =>
+  useQueries(
+    drinks.map((drink) => ({
+      queryKey: [DRINK_EVALUATION_KEY, drink?.id],
+      queryFn: () =>
+        DrinkEvaluationService.getDrinksEvaluation({ id: drink?.id }),
+    }))
+  );
