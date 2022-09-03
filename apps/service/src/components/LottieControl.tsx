8ca17b711a909dc51b3336ac388dc15b2a840055
otterji <youngsdg1@gmail.com>
2022-09-03 15:40:33 +0900

    fix: lottie hot fix

--- apps/service/src/components/LottieControl.tsx
+++ apps/service/src/components/LottieControl.tsx
@@ -3,28 +3,20 @@ import React from "react";
 import Lottie from "react-lottie";
 import * as lottieFile from "./lottie.json";
 
-export default class LottieControl extends React.Component {
-  constructor(props: {} | Readonly<{}>) {
-    super(props);
-    this.state = { isStopped: false, isPaused: false };
-  }
-
-  render() {
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: lottieFile,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice",
-      },
-    };
-
-    return (
-      <Wrapper>
-        <Lottie options={defaultOptions} height={400} width={400} />
-      </Wrapper>
-    );
-  }
+function LottieControl() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: lottieFile,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <Wrapper>
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
@@ -33,3 +25,5 @@ const Wrapper = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
 `;
+
+export default LottieControl;
